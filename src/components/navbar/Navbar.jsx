9a5a3e1726d537dcbe5449d40eb0e8683940fa30
Rosen Georgiev <rosengeorgiew1@gmail.com>
2024-08-12 16:26:48 +0300

    Update Navbar.jsx

--- src/components/navbar/Navbar.jsx
+++ src/components/navbar/Navbar.jsx
@@ -1,4 +1,5 @@
 import React from "react";
+import "../../styles/navbar.css";
 
 const Navbar = () => {
   return (
@@ -6,10 +7,18 @@ const Navbar = () => {
       <div className="logo">Rosen Georgiev</div>
       <div>
         <ul className="nav-links">
-          <li>About</li>
-          <li>Experience</li>
-          <li>Projects</li>
-          <li>Contact</li>
+          <li>
+            About
+          </li>
+          <li>
+            Experience
+          </li>
+          <li>
+            Projects
+          </li>
+          <li>
+            Contact
+          </li>
         </ul>
       </div>
     </nav>
