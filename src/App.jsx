f705db57abc096e06c685b4dc515987839dce077
Rosen Georgiev <rosengeorgiew1@gmail.com>
2024-08-13 15:20:11 +0300

    Project update

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,15 @@
 import React from 'react'
+import Main from './components/Main'
 
 const App = () => {
   return (
-    <div>App</div>
+    <div className='relative h-full overflow-y-auto antialiased'>
+      <div className='fixed inset-0 bgfixed bg-cover bg-center bg-img'>
+        <div className='relative z-10 flex flex-col items-center p-4 space-y-8 container mx-auto'>
+          <Main />
+        </div>
+      </div>
+      </div>
   )
 }
 
