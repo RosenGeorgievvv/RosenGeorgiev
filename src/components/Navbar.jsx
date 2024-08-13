83422f11f77d7bc1fd863d7448475940e9466df3
Rosen Georgiev <rosengeorgiew1@gmail.com>
2024-08-13 15:36:12 +0300

    Update Navbar.jsx

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -24,7 +24,12 @@ const handleLinkClick = (e, href) =>{
 }
 
   return (
-    <div>Navbar</div>
+    <div>
+        <nav className='fixed left-0 right-0 top-4 z-50'>
+            {/* Desktop Menu */}
+            <div className='mx-auto hidden max-w-2xl items-center justify-center rounded-lg border border-stone-50/30 bg-black/20 py-3 backdrop-blur-lg lg:flex'></div>
+        </nav>
+    </div>
   )
 }
 
