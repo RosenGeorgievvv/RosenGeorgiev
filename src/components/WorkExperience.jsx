7697a0e11e67e9277e0e01a3ce7024dbbd3fea70
Rosen Georgiev <rosengeorgiew1@gmail.com>
2024-08-27 12:57:48 +0300

    Update WorkExperience.jsx
    Work Experience component finished.

--- src/components/WorkExperience.jsx
+++ src/components/WorkExperience.jsx
@@ -2,7 +2,19 @@ import { EXPERIENCES } from "../constants"
 
 const WorkExperience = () => {
   return (
-    <div>WorkExperience</div>
+    <section className="pt-20" id="work">
+        <h2 className="text-center text-4xl font-semibold tracking-tighter">Work Experience</h2>
+        <div className="space-y-8 p-10">
+            {EXPERIENCES.map((experience, index) => (
+                <div key={index} className="rounded-xl border border-stone-50/30 bg-white/10 p-4">
+                    <h3 className="text-2xl font-semibold">{experience.title}</h3>
+                    <p className="text-xl">{experience.company}</p>
+                    <p className="text-sm text-stone-300">{experience.duration}</p>
+                    <p className="mt-2 text-base">{experience.description}</p>
+                </div>
+            ))}
+        </div>
+    </section>
   )
 }
 
