70d9b142a8adccdcddbe8b47f90249ac353141d0
Rosen Georgiev <rosengeorgiew1@gmail.com>
2024-08-27 13:29:26 +0300

    Update Projects.jsx

--- src/components/Projects.jsx
+++ src/components/Projects.jsx
@@ -1,17 +1,30 @@
 import { MdArrowOutward } from "react-icons/md";
 import { PROJECTS } from "../constants";
+import { motion } from "framer-motion";
 
 const Projects = () => {
   return (
     <section className="pt-20" id="projects">
-      <h2 className="mb-8 text-center text-3xl lg:text-4xl">Projects</h2>
+      <motion.h2
+        initial={{ opacity: 0, y: -20 }}
+        whileInView={{ opacity: 1, y: 0 }}
+        transition={{ duration: 0.8 }}
+        className="mb-8 text-center text-3xl lg:text-4xl"
+      >
+        Projects
+      </motion.h2>
       <div className="grid grid-cols-1 gap-4 md:grid-cols-2 lg:grid-cols-3">
         {PROJECTS.map((project) => (
-          <div
+          <motion.div
+            initial={{ opacity: 0, scale: 0.9 }}
+            whileInView={{ opacity: 1, scale: 1 }}
+            transition={{ duration: 0.5 }}
+            whileHover={{ scale: 1.05 }}
             key={project.id}
             className="group relative overflow-hidden rounded-3xl"
           >
-            <img
+            <motion.img
+            whileHover={{scale: 1.1}}
               src={project.image}
               alt={project.name}
               className="h-full w-full object-cover transition-transform duration-500 group-hover:scale-110"
@@ -20,7 +33,7 @@ const Projects = () => {
               <h3 className="mb-2 text-xl text-black">{project.name}</h3>
               <p className="mb-12 p-4 text-black">{project.description}</p>
               <div className="flex space-x-4">
-              <a
+                <a
                   href={project.demoLink}
                   target="_blank"
                   rel="noopener noreferrer"
@@ -39,7 +52,7 @@ const Projects = () => {
                 </a>
               </div>
             </div>
-          </div>
+          </motion.div>
         ))}
       </div>
     </section>
