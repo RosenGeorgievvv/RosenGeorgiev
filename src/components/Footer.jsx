e2ed49c727c1c67c71c722515f7a9d5e4258bb4e
Rosen Georgiev <rosengeorgiew1@gmail.com>
2024-08-27 14:17:25 +0300

    Project finished
    This last update covers all animations through my website. Go check it out!

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -1,13 +1,14 @@
-import { SOCIAL_MEDIA_LINKS } from "../constants"
+import { SOCIAL_MEDIA_LINKS } from "../constants";
+import {motion} from 'framer-motion';
 
 const Footer = () => {
   return (
     <div className="mb-8 mt-20">
         <div className="flex items-center justify-center gap-8">
             {SOCIAL_MEDIA_LINKS.map((link, index) => (
-                <a key={index} href={link.href} target="_blank" rel="noopener noreferrer" className="hover:text-green-300">
+                <motion.a initial={{opacity: 0}} whileInView={{ opacity: 1}} transition={{ duration: 0.2, delay: index * 0.5}} key={index} href={link.href} target="_blank" rel="noopener noreferrer" className="hover:text-green-300">
                     {link.icon}
-                </a>
+                </motion.a>
             ))}
         </div>
     </div>
